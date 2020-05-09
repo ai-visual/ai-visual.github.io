e818d89016018c900ceb35f6403d1a7840adad58
santa-sukitoku <santanaruse@gmail.com>
2020-05-10 05:17:42 +0900

    feat: add project files

---
+++ pages/menu/contact.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <div class="contact">
+      <p>Contact</p>
+    </div>
+    <div class="logo">
+      <a href="https://cclab.sfc.keio.ac.jp/">
+        <img src="~/assets/img/cclab-logo.png" alt />
+      </a>
+    </div>
+    <div class="mail">
+      <a href="mailto:sfc.cclab&#64;gmail.com">sfc.cclab&#64;gmail.com</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import url("https://use.typekit.net/bvf7soa.css");
+
+.contact {
+  padding-bottom: 91px;
+}
+
+.contact p {
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  width: 900px;
+  font-family: neue-haas-grotesk-text, sans-serif;
+  font-style: normal;
+  font-size: 40px;
+  font-style: normal;
+  letter-spacing: 0px;
+  color: #ffffff;
+  opacity: 1;
+  text-decoration: none;
+}
+
+.logo {
+  text-align: center;
+  padding-bottom: 92px;
+}
+
+.logo img {
+  text-align: center;
+  width: 541px;
+}
+
+.mail {
+  text-align: center;
+  padding-bottom: 112px;
+}
+
+.mail a {
+  text-align: center;
+  font-family: neue-haas-grotesk-text, sans-serif;
+  font-size: 20px;
+  position: relative;
+  display: inline-block;
+  text-decoration: none;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: 0px;
+  color: #ffffff;
+  opacity: 1;
+}
+
+.mail a::after {
+  position: absolute;
+  bottom: -4px;
+  left: 0;
+  content: "";
+  width: 100%;
+  height: 2px;
+  background: #ffffff;
+  transform: scale(0, 1);
+  transform-origin: left top;
+  transition: transform 0.3s;
+}
+
+.mail a:hover::after {
+  transform: scale(1, 1);
+}
+</style>
---
+++ pages/menu/members.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container">
+    <div class="members">
+      <p>Members</p>
+    </div>
+    <div class="membersList">
+      <p class="txt">
+        Scott Allen :
+        <a href="https://scottallen.ws/">scottallen.ws</a>
+        <br />Yusuke Yamada :
+        <a href="https://portfolio.yamad07.world/">portfolio.yamad07.world/yamad07</a>
+        <br />Reo Anzai :
+        <a href="https://reoanzai.com/">reoanzai.com</a>
+        <br />Ryo Nishikado :
+        <a href="https://ryo-simon-mf.github.io/">ryo-simon-mf.github.io/</a>
+        <br />Santa Naruse :
+        <a href="https://santa-sukitoku.github.io/">santa-sukitoku.github.io/</a>
+        <br />Aina Ono
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import url("https://use.typekit.net/bvf7soa.css");
+
+.members p {
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  width: 900px;
+  font-family: neue-haas-grotesk-text, sans-serif;
+  font-style: normal;
+  font-size: 40px;
+  font-style: normal;
+  letter-spacing: 0px;
+  color: #ffffff;
+  opacity: 1;
+  text-decoration: none;
+  padding-bottom: 146px;
+}
+
+.membersList {
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-bottom: 292px;
+}
+
+.membersList p {
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  line-height: 80px;
+  color: #ffffff;
+  width: 900px;
+  font-family: neue-haas-grotesk-text, sans-serif;
+  font-style: normal;
+  font-size: 20px;
+  font-style: normal;
+  letter-spacing: 0px;
+  color: #ffffff;
+  opacity: 1;
+  text-decoration: none;
+  padding-bottom: 15px;
+}
+
+.membersList a {
+  font-family: neue-haas-grotesk-text, sans-serif;
+  font-size: 20px;
+  line-height: 30px;
+  position: relative;
+  display: inline-block;
+  text-decoration: none;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: 0px;
+  color: #ffffff;
+  opacity: 1;
+}
+
+.membersList a::after {
+  position: absolute;
+  bottom: -4px;
+  left: 0;
+  content: "";
+  width: 100%;
+  height: 2px;
+  background: #ffffff;
+  transform: scale(0, 1);
+  transform-origin: left top;
+  transition: transform 0.3s;
+}
+
+.membersList a:hover::after {
+  transform: scale(1, 1);
+}
+</style>
